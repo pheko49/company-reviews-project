f83e32ed80a7424f0b16fc631b12be24a1be2ca3
pheko49 <phekofm49.pm@gmail.com>
2026-06-05 23:02:28 +0200

    Link pages to side bar and add plotly chart to Analytics page

--- src/dashboard/streamlit_app.py
+++ src/dashboard/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
+import plotly.express as px
 
 # page config
 st.set_page_config(
@@ -26,36 +27,58 @@ page = st.sidebar.selectbox(
         'Home', 'Analytics', 'Company Search'
     ]
 )
+if page == 'Home':
+    # title
+    st.title('📊 Company Reviews Dashboard')
 
-# title
-st.title('📊 Company Reviews Dashboard')
+    #kpis
+    total_companies = len(df)
 
-#kpis
-total_companies = len(df)
+    average_rating = round(df['rating'].mean(), 2)
 
-average_rating = round(df['rating'].mean(), 2)
+    total_reviews = df['reviews_count'].sum()
 
-total_reviews = df['reviews_count'].sum()
+    col1, col2, col3 = st.columns(3)
 
-col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(
+            label='Total Companies',
+            value=total_companies
+        )
 
-with col1:
-    st.metric(
-        label='Total Companies',
-        value=total_companies
+    with col2:
+        st.metric(
+            label='Average Rating',
+        value=average_rating
+        )
+
+    with col3:
+        st.metric(
+            label='Total Reviews',
+            value=f'{total_reviews:,}'
+        )
+
+    # table/dataframe
+    st.dataframe(df)
+
+elif page == 'Analytics':
+
+    st.title('📈 Analytics Dashboard')
+
+    top_rated = requests.get(
+        f'{API_URL}/top-rated-companies'
+    ).json()
+
+    top_rated_df = pd.DataFrame(top_rated)
+
+    fig = px.bar(
+        top_rated_df,
+        x='company_name',
+        y='rating',
+        title='Top Rated Companies'
     )
 
-with col2:
-    st.metric(
-        label='Average Rating',
-       value=average_rating
+    st.plotly_chart(
+        fig,
+        use_container_width=True
     )
-
-with col3:
-    st.metric(
-        label='Total Reviews',
-        value=f'{total_reviews:,}'
-    )
-
-# table/dataframe
-st.dataframe(df)
